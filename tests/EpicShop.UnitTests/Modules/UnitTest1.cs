0c6a3fc545ffcb9480b28174a35a88a59c728c85
thivy <thivyakanth@live.com>
2017-09-16 14:51:44 +1200

    initial application commit

---
+++ tests/EpicShop.UnitTests/Modules/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EpicShop.UnitTests.Modules
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
