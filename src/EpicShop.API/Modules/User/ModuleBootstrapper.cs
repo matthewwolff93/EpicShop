a8050678842e396f67cb711f79d60f8439d3262b
thivy <thivyakanth@live.com>
2017-10-02 18:34:52 +1300

    Adding API user

---
+++ src/EpicShop.API/Modules/User/ModuleBootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using EpicShop.Core.Infrastructure.Extensions;
+using EpicShop.Core.Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EpicShop.API.Modules.User
+{
+    public class ModuleBootstrapper : IModuleBootstrapper
+    {
+        public void Run(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IUserManager, UserManager>();
+        }
+    }
+}
---
+++ src/EpicShop.API/Modules/User/UserManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Claims;
+using EpicShop.Core.Infrastructure.Services;
+using Microsoft.AspNetCore.Http;
+
+namespace EpicShop.API.Modules.User
+{
+    public class UserManager : IUserManager
+    {
+        private readonly IHttpContextAccessor _httpContext;
+
+        public UserManager(IHttpContextAccessor httpContext)
+        {
+            _httpContext = httpContext;
+        }
+
+        public string ResolveUserName()
+        {
+            var userId = _httpContext.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier);
+
+            return "API user";
+        }
+    }
+}
